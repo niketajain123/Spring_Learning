4e21e12bc513639e0beebc1384525c83ddd7fd6a
niketajain123 <niketajain236@gmail.com>
2025-02-21 22:21:15 +0530

    [Niketa Jain]Add. HelloController added.

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from BridgeLabz");
+        return "hello"; // This corresponds to the hello.html Thymeleaf template
+    }
+}
